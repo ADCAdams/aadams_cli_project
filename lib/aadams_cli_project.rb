bb366a2d3207260beaef9ea4286502ac3a27c81e
Andy Adams <andrewdcadams@gmail.com>
2020-08-19 22:32:52 +0000

    initial setup

---
+++ lib/aadams_cli_project.rb
@@ -0,0 +1,6 @@
+require "aadams_cli_project/version"
+
+module AadamsCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/aadams_cli_project/version.rb
@@ -0,0 +1,3 @@
+module AadamsCliProject
+  VERSION = "0.1.0"
+end
